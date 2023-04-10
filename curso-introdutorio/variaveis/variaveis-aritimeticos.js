c98ab46236c57252cce0af777f5101727760fe3c
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-04-10 19:00:31 -0300

    Adicionando exercícios

--- curso-introdutorio/variaveis/variaveis-aritimeticos.js
+++ curso-introdutorio/variaveis/variaveis-aritimeticos.js
@@ -3,16 +3,18 @@ let x = 10, y = 2;
 
 let resultado = x + y;
 
-console.log(resultado); //12
+console.log(`A soma de ${x} e ${y} é ${resultado} \n`); //12
 
 resultado = x - y; 
 
-console.log(resultado); //8
+console.log(`A subtração de ${x} e ${y} é ${resultado} \n`); //8
 
 resultado = x * y; 
 
-console.log(resultado); //20
+console.log(`A multiplicação de ${x} e ${y} é ${resultado} \n`); //20
 
 resultado = x / y;
 
-console.log(resultado); //5
+console.log(`A divisão de ${x} e ${y} é ${resultado} `); //5
+
+// \n pula linha em JavaScript
---
+++ curso-introdutorio/variaveis/variaveis-concatenacao.js
@@ -0,0 +1,6 @@
+let nome = " Jeferson ";
+let sobreNome = " Vieira";
+
+let nomeCompleto = nome + sobreNome;
+
+console.log(nomeCompleto);
