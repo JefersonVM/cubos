8b5f82a10cae95c276d3c9f17075ed1b7ce02b4e
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-04-11 17:17:32 -0300

    Adicionando exercícios de condicionais

---
+++ curso-introdutorio/condicionais/exemplo.js
@@ -0,0 +1,9 @@
+const temIngresso = true;
+const censura = 16;
+const idade = 16;
+
+if (idade >= censura && temIngresso) {
+    console.log(`Pode entrar no cinema!`);
+}else {
+    console.log(`Não pode entrar no cinema!`);
+}
---
+++ curso-introdutorio/condicionais/if.js
@@ -0,0 +1,22 @@
+const idade = 17;
+
+if(idade < 18) {
+
+   
+ console.log(`Você é menor de idade.`);
+   
+}else if (idade <= 65) {
+
+    console.log(`Adulta`);
+}else {
+    console.log(`Idosa`);
+}
+
+                                 
+   
+
+
+console.log(`Fim do código!`);
+
+
+ 
