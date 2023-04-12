595ea9b268d0fef0e8fdca855d3acc7b2d618831
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-04-11 21:19:32 -0300

    Exercício 02

--- curso-introdutorio/condicionais/exercicio02.js
+++ curso-introdutorio/condicionais/exercicio02.js
@@ -1,13 +1,19 @@
 const valorDoProduto = 100;
-const quantDeParcelas = 3;
+const quantDeParcelas = 12;
 const desconto = valorDoProduto * 0.1;
+const montante = valorDoProduto * (1 + 1.01) ** quantDeParcelas;
 
 if (quantDeParcelas === 1){
     //à vista - com 10% de desconto.
-    console.log(`Você deve pagar R$${(valorDoProduto - desconto).toFixed(2)}`);
+    console.log(`Você deve pagar R$ ${(valorDoProduto - desconto).toFixed(2)}`);
 
-}else {
-    //parcelado
+}else  if (quantDeParcelas >= 7 &&  quantDeParcelas <= 12){
+    //parcelado com juros
+    console.log(`Você vai pagar R$ ${quantDeParcelas} parcelas de ${((valorDoProduto / quantDeParcelas + montante / quantDeParcelas)).toFixed(2)}`);
+}else if (quantDeParcelas >= 2 && quantDeParcelas <= 6) {
+    //parcelado sem juros
     console.log(`Você vai pagar ${quantDeParcelas} parcelas de R$ ${(valorDoProduto / quantDeParcelas).toFixed(2)}.`);
-
+   
+}else {
+    console.log(`Quantidade de parcelas inválida!`);
 }
