3cf8d787bb576d26fd16d80bf67bb5f9012bd99b
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-04-17 18:40:21 -0300

    Adicionando exercício 02

---
+++ conteudos/objetos/exercicio02.js
@@ -0,0 +1,29 @@
+
+
+
+
+const cartaoCliente = {
+
+    nomeDoCliente: "Jeferson",
+    idadeDoCliente: 41,
+    produtosConsumidos: [
+        {
+            nome: `Arroz`,
+            precoUni: 20.53,
+            qtd: 2
+        },
+        {
+            nome: `Feijão`,
+            precoUni: 14.28,
+            qtd: 2
+        },
+        {
+            nome: `Macarrão`,
+            precoUni: 10.90,
+            qtd: 2,
+
+        }
+    ]
+};
+
+console.log(cartaoCliente);
