fc2ad2e9a13dd080b9895fc929077a418b698e9a
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-04-17 17:58:47 -0300

    Adicionando shorthand

---
+++ conteudos/objetos/shorthand.js
@@ -0,0 +1,20 @@
+
+const carro = {
+    marca: `Chevrolet`,
+    modelo: `Cruzer`,
+    ano: 2020,
+    potencia: 2.4,
+}
+
+
+
+
+const pessoa = {
+    nome: `Jeferson`,
+    altura: 1.86,
+    peso: 86,
+    carro
+   
+};
+
+console.log(pessoa);
