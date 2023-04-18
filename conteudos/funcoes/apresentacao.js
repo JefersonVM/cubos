f64e07869601842dde8c7e9e508503b1b07b8861
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-04-18 15:43:10 -0300

    adicionando aquivos

---
+++ conteudos/funcoes/apresentacao.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome: `Jeferson`,
+    idade: 41,
+    profissao: `Dev`
+};
+
+const pessoa2 = {
+    nome: `Pedro`,
+    idade: 45,
+    profissao: `Motoca`
+};
+
+console.log(`Meu nome é ${pessoa.nome} tenho ${pessoa.idade} anos e sou ${pessoa.profissao}.`);
