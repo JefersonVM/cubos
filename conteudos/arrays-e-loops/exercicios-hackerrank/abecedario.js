853fb62bdb005ec87d7c8db31293bec2e39a3703
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-04-21 23:50:31 -0300

    Adicionando arquivo abecedário

---
+++ conteudos/arrays-e-loops/exercicios-hackerrank/abecedario.js
@@ -0,0 +1,20 @@
+    
+    let letra = `F`;
+
+    const palavras = [`Flauta`, `Abacaxi`, `Ameixa`, `Abacate`];
+
+    
+    let perde = 0;
+    
+    for (primeiraLetra of palavras){
+        
+                
+        if (primeiraLetra[0] !== letra){
+            
+            perde += 1;          
+            
+            
+            }
+    }
+    console.log(perde);
+    
