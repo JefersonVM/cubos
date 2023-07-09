e0690aaa7dfdba00f01d48a19c692d6062398a45
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-07-08 23:30:02 -0300

    Finalizando exercício de EStado-03

--- conteudos/front-end/estados/aula03/estado-03/src/pages/Main/index.js
+++ conteudos/front-end/estados/aula03/estado-03/src/pages/Main/index.js
@@ -1,28 +1,90 @@
 import "./style.css";
+import { useState } from "react";
 
 function Main() {
+  const [tasks, setTasks] = useState([]);
+
+  function handleAddTask(event) {
+    const content = event.target.value;
+
+    if (event.key !== "Enter" || content === "") {
+      return;
+    }
+
+    const localTasks = [...tasks];
+
+    const newTask = {
+      id: localTasks.length ? localTasks[localTasks.length - 1].id + 1 : 1,
+      name: content,
+      done: false,
+    };
+
+    localTasks.push(newTask);
+
+    setTasks(localTasks);
+
+    event.target.value = "";
+  }
+
+  function handleDeleteTask(taskId) {
+    const localTasks = [...tasks];
+
+    const taskIndex = localTasks.findIndex((task) => task.id === taskId);
+
+    if (taskIndex === -1) {
+      return;
+    }
+
+    localTasks.splice(taskIndex, 1);
+
+    setTasks(localTasks);
+
+    console.log(taskIndex);
+  }
+
+  function handleChangeStatus(taskId) {
+    const localTasks = [...tasks];
+
+    const findTask = localTasks.find((task) => task.id === taskId);
+
+    if (!findTask) {
+      return;
+    }
+
+    findTask.done = !findTask.done;
+
+    setTasks(localTasks);
+  }
   return (
     <div className="container">
       <div>
-        <input type="text" placeholder="Nova tarefa" />
+        <input
+          type="text"
+          placeholder="Nova tarefa"
+          onKeyDown={(event) => {
+            handleAddTask(event);
+          }}
+        />
       </div>
 
       <div>
         <ul>
-          <li><span>Texto</span></li>
-          <button className='btn-del'>X</button>
-
-          <li><span>Texto</span></li>
-          <button className='btn-del'>X</button>
-
-          <li><span>Texto</span></li>
-          <button className='btn-del'>X</button>
-
-          <li><span>Texto</span></li>
-          <button className='btn-del'>X</button>
-
-          <li><span>Texto</span></li>
-          <button className='btn-del'>X</button>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <span
+                className={`${task.done && "task-done"}`}
+                onClick={() => handleChangeStatus(task.id)}
+              >
+                {task.name}
+              </span>
+              <button
+                className="btn-del"
+                onClick={() => handleDeleteTask(task.id)}
+              >
+                X
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
