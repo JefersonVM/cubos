c831c2c7aff7b7d6b9c5145a091e910b89e8f5a1
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-08-01 19:27:49 -0300

    Adicionando a finalização dos exemplos

---
+++ conteudos/back-end/Primeiro Servidor Express/primeiro-servidor-express/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/home", (requisicao, resposta) => {
+  resposta.send("Olá... Esse é o meu primeiro servidor com o express");
+});
+
+app.get("/", (requisicao, resposta) => {
+  resposta.send("Essa é a minha rota principal");
+});
+
+app.get("/array", (requisicao, resposta) => {
+    const array = [1, 2, 3, 4, 5];
+  resposta.send(array);
+});
+
+app.listen(3000);
