58ff0d97d6fc6ae675e2b564ee7d6270086d60af
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-08-02 00:36:42 -0300

    Adicionando exemplo da aula

---
+++ conteudos/back-end/Exercícios da aula/exercicio01/src/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+
+const usuarios = [
+  {
+    id: 11,
+    nome: "joão",
+    idade: 23,
+  },
+  {
+    id: 2,
+    nome: "maria",
+    idade: 18,
+  },
+  {
+    id: 4,
+    nome: "ana",
+    idade: 30,
+  },
+  {
+    id: 1,
+    nome: "rodrigo",
+    idade: 17,
+  },
+  {
+    id: 123,
+    nome: "rodrigo",
+    idade: 17,
+  },
+];
+
+app.get("/usuarios", (req, res) => {
+  res.send(usuarios);
+});
+
+app.listen(3000, () => {
+    console.log('Servidor inicializado na porta 3000');
+});
