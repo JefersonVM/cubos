260fca5f9c44664b5c79f73b756895254cedd42f
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-08-03 15:11:35 -0300

    Adicionando exemplos de useEffect

--- conteudos/front-end/useeffect-e-ciclo-de-vida/app01/src/pages/Main/index.js
+++ conteudos/front-end/useeffect-e-ciclo-de-vida/app01/src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import "./style.css";
 
 function Main() {
@@ -6,22 +6,42 @@ function Main() {
   const [count2, setCount2] = useState(0);
 
   useEffect(() => {
-    console.log('useEffect sem array de dependências ');
+    console.log("useEffect sem array de dependências ");
   });
-  
-  return <div className="container">
-   <div className="container-count">
-    <h1>Contador 1</h1>
-   <h2>{count}</h2>
-    <button onClick={() => setCount(count + 1)}>Incrementar 1</button>
-   </div>
 
-   <div className="container-count">
-    <h1>Contador 2</h1>
-   <h2>{count2}</h2>
-    <button onClick={() => setCount2(count2 + 1)}>Incrementar 2</button>
-   </div>
-  </div>;
+  useEffect(() => {
+    console.log("useEffect com array de dependências vazio ");
+  }, []);
+
+  useEffect(() => {
+    console.log('useEffect com array contendo o estado count');
+  }, [count]);
+
+  useEffect(() => {
+    console.log('useEffect com array contendo o estado count2');
+  }, [count2]);
+
+  useEffect(() => {
+    console.log('useEffect com array contendo o estado count e count2');
+  }, [count, count2]);
+
+
+
+  return (
+    <div className="container">
+      <div className="container-count">
+        <h1>Contador 1</h1>
+        <h2>{count}</h2>
+        <button onClick={() => setCount(count + 1)}>Incrementar 1</button>
+      </div>
+
+      <div className="container-count">
+        <h1>Contador 2</h1>
+        <h2>{count2}</h2>
+        <button onClick={() => setCount2(count2 + 1)}>Incrementar 2</button>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
