393e1ddd4eafe438891f6a2711c7b965db13fccf
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-08-10 01:41:09 -0300

    Atualizando os exemplos da aula

--- conteudos/back-end/API REST/aula-api-rest/src/controladores/instrutores.js
+++ conteudos/back-end/API REST/aula-api-rest/src/controladores/instrutores.js
@@ -1,5 +1,5 @@
-const { instrutores } = require("../bancodedados");
-let {identificadorInstrutor} = require ("../bancodedados");
+let { instrutores, identificadorInstrutor } = require("../bancodedados");
+
 
 const listarInstrutores = (req, res) => {
   return res.status(200).json(instrutores);
@@ -20,27 +20,95 @@ const obterInstrutor = (req, res) => {
 };
 
 const cadastrarInstrutor = (req, res) => {
-    const {nome, email, status} = req.body;
+  const { nome, email, status } = req.body;
 
-    if(!nome){
-        return res.status(400).json({mensagem: 'O nome é obrigatório'});
-    }
+  if (!nome) {
+    return res.status(400).json({ mensagem: "O nome é obrigatório" });
+  }
 
-    if(!email){
-        return res.status(400).json({mensagem: 'O emailé obrigatório'});
-    }
+  if (!email) {
+    return res.status(400).json({ mensagem: "O emailé obrigatório" });
+  }
 
-    const instrutor = {
-        id: identificadorInstrutor++,
-        nome,
-        email,
-        status: status ?? true
+  const instrutor = {
+    id: identificadorInstrutor++,
+    nome,
+    email,
+    status: status ?? true,
+  };
 
-    }
+  instrutores.push(instrutor);
 
-    instrutores.push(instrutor);
-
-    return res.status(201).json(instrutor);
+  return res.status(201).json(instrutor);
 };
 
-module.exports = { listarInstrutores, obterInstrutor, cadastrarInstrutor };
+const atualizarInstrutor = (req, res) => {
+  const { id } = req.params;
+  const { nome, email, status } = req.body;
+
+  if (!nome) {
+    return res.status(400).json({ mensagem: "O nome é obrigatório" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ mensagem: "O email é obrigatório" });
+  }
+
+  const instrutor = instrutores.find((instrutor) => {
+    return instrutor.id === Number(id);
+  });
+
+  if (!instrutor) {
+    return res.status(404).json({ mensagem: "Instrutor não encontrado." });
+  }
+
+  instrutor.nome = nome;
+  instrutor.email = email;
+  instrutor.status = status;
+
+  return res.status(204).send();
+};
+
+const atualizarStatusInstrutor = (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  const instrutor = instrutores.find((instrutor) => {
+    return instrutor.id === Number(id);
+  });
+
+  if (!instrutor) {
+    return res.status(404).json({ mensagem: "Instrutor não encontrado." });
+  }
+
+  instrutor.status = status;
+
+  return res.status(204).send();
+};
+
+const excluirInstrutor = (req, res) => {
+  const { id } = req.params;
+
+  const instrutor = instrutores.find((instrutor) => {
+    return instrutor.id === Number(id);
+  });
+  if (!instrutor) {
+    return res.status(404).json({ mensagem: "O instrutor não existe." });
+  }
+
+  instrutores = instrutores.filter((instrutor) => {
+    return instrutor.id !== Number(id);
+  });
+  return res.status(204).send(); 
+};
+
+
+
+module.exports = {
+  listarInstrutores,
+  obterInstrutor,
+  cadastrarInstrutor,
+  atualizarInstrutor,
+  atualizarStatusInstrutor,
+  excluirInstrutor,
+};
