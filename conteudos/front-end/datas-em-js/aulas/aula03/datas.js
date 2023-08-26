e2733ae19d7081e1ee1e7d69bf9785177b71e5e7
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-08-25 22:27:03 -0300

    Finalizando exemplo

---
+++ conteudos/front-end/datas-em-js/aulas/aula03/datas.js
@@ -0,0 +1,11 @@
+const finalDaCopa = new Date(2002, 5, 30, 8);
+
+// const inicioTimestamp = +finalDaCopa;
+
+// const primeiroGolTimestamp = inicioTimestamp + (45+15+22)*60*1000;
+
+// const primeiroGolDate = new Date(primeiroGolTimestamp);
+
+// console.log(primeiroGolDate);
+
+console.log(new Date(+finalDaCopa + (45 + 15 + 38)*60*1000));
