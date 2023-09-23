387c315c6a7cfa3b6ab9139364c1790e506e3032
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-09-23 00:35:07 -0300

    Finalizando o exemplo de aula

---
+++ conteudos/front-end/design-systems-e-bibliotecas/classe02/src/pages/Main/index.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+// import CustomCard from "../../components/CustomCard";
+// import CustomTable from "../../components/CustomTable";
+// import CustomAutoComplete from "../../components/CustomAutoComplete";
+// import CustomStepper from "../../components/CustomStepper";
+// import AddUser from "../../components/AddUser";
+// import UserBackdrop from "../../components/UserBackdrop";
+import CustomButton from "../../components/CustomButton";
+
+function Main() {
+  return (
+    < div className="container-main">
+      <CustomButton/>
+      {/* <UserBackdrop/> */}
+      {/* <AddUser/> */}
+      {/* <CustomStepper/> */}
+      {/* <CustomAutoComplete/> */}
+      {/* <CustomTable/> */}
+      {/* <CustomCard/> */}
+    </div>
+  );
+}
+
+export default Main;
