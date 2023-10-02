dbc1947f1b3d720915d3e77727ee4a26af399ed1
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-10-02 16:47:29 -0300

    Finalizando exemplo de aula

--- conteudos/front-end/aula-responsividade-fluido/src/components/Menu/index.js
+++ conteudos/front-end/aula-responsividade-fluido/src/components/Menu/index.js
@@ -1,16 +1,41 @@
-import './style.css';
+import { useEffect, useState } from "react";
+import CloseIcon from "../../assets/close.svg";
+import MenuIcon from "../../assets/menu.svg";
+import "./style.css";
 
-function Menu () {
+function Menu() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    document.querySelector("body").style.overflow = open ? "hidden" : "initial";
+  }, [open]);
   return (
-    <div>
-      <ul className='menu'>
+    <div className={`${open && "fullscreen-menu"}`}>
+      {open ? (
+        <img
+          className="icon-close"
+          style={{ display: `${open} ? 'block' : 'none'` }}
+          src={CloseIcon}
+          alt="close-icon"
+          onClick={() => setOpen(false)}
+        />
+      ) : (
+        <img
+          className="icon-menu"
+          src={MenuIcon}
+          alt="menu icon"
+          onClick={() => setOpen(true)}
+        />
+      )}
+
+      <ul className="menu">
         <li>Acão</li>
         <li>Comédia</li>
         <li>Drama</li>
         <li>Documentários</li>
         <li>Infantis</li>
       </ul>
-      </div>
+    </div>
   );
 }
 
